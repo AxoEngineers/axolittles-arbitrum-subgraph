cbba6c26c8afd842c133aff6432154d219a43de5
michael <michael.r.klear@gmail.com>
2022-06-29 11:38:17 -0700

    code i forgot to push earlier

--- src/mapping.ts
+++ src/mapping.ts
@@ -10,65 +10,9 @@ import {
   SetTrustedRemote,
   Transfer
 } from "../generated/Axolittles/Axolittles"
-import { ExampleEntity } from "../generated/schema"
+import { Axolittle, Tran, TransferTransaction, AxoHolder } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract._baseTokenURI(...)
-  // - contract.balanceOf(...)
-  // - contract.estimateSendFee(...)
-  // - contract.failedMessages(...)
-  // - contract.getApproved(...)
-  // - contract.getConfig(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.isTrustedRemote(...)
-  // - contract.lzEndpoint(...)
-  // - contract.maxItems(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenURI(...)
-  // - contract.trustedRemoteLookup(...)
-}
+export function handleApproval(event: Approval): void {}
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
@@ -82,4 +26,60 @@ export function handleSendToChain(event: SendToChain): void {}
 
 export function handleSetTrustedRemote(event: SetTrustedRemote): void {}
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: Transfer): void {
+  let fromAccount = AxoHolder.load(event.params.from.toHex())
+  if (fromAccount == null) {
+    fromAccount = new AxoHolder(event.params.from.toHex())
+    fromAccount.firstActiveBlock = event.block.number
+  }
+
+  let toAccount = AxoHolder.load(event.params.to.toHex())
+  if (toAccount == null) {
+    toAccount = new AxoHolder(event.params.to.toHex())
+    toAccount.firstActiveBlock = event.block.number
+  }
+
+  let axo = Axolittle.load(event.params.tokenId.toString())
+  if (axo == null) {
+    axo = new Axolittle(event.params.tokenId.toString())
+    axo.bridgeBlock = event.block.number
+  }
+  axo.owner = event.params.to.toHex()
+
+  fromAccount.lastActiveBlock = event.block.number
+  toAccount.lastActiveBlock = event.block.number
+  
+  let txnString = event.transaction.hash.toHexString()
+  let tokenString = event.params.tokenId.toString()
+  let fromString = event.params.from.toHexString()
+  let toString = event.params.to.toHexString()
+  //this complex id is needed because
+  //some transactions include multiple transfers of the same
+  //token, for example nftx.io staking
+  let transfer_id = txnString + tokenString + fromString + toString
+  let transfer = new Tran(transfer_id)
+  transfer.from = event.params.from.toHex()
+  transfer.to = event.params.to.toHex()
+  transfer.token_id = event.params.tokenId
+  transfer.blockHeight = event.block.number
+  transfer.timestamp = event.block.timestamp
+
+  let transaction = TransferTransaction.load(event.transaction.hash.toHex())
+  if (transaction == null) {
+    // need to create instance
+    transaction = new TransferTransaction(event.transaction.hash.toHex())
+    transaction.nTransfers = 0
+  }
+  transaction.valueWei = event.transaction.value
+  // incriment for each axo transfered.
+  transaction.nTransfers += 1
+  
+  transfer.transaction = event.transaction.hash.toHex()
+  transfer.value = event.transaction.value
+  fromAccount.save()
+  toAccount.save()
+  transfer.save()
+  axo.save()
+  transaction.save()
+
+}
